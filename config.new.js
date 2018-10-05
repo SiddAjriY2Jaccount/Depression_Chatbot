b42b6240fa9a8c745e6d10bfe104e8fe311a230b
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2018-10-06 01:01:27 +0530

    latest Mac code works

---
+++ config.new.js
@@ -0,0 +1,27 @@
+// The attention word to wake up the robot.
+exports.attentionWord ='watson';
+
+// You can change the voice of the robot to your favorite voice.
+exports.voice = 'en-US_AllisonVoice';
+// Some of the available options are:
+// en-US_AllisonVoice
+// en-US_LisaVoice
+// en-US_MichaelVoice (the default)
+
+
+// Credentials for Watson Speech to Text service
+exports.STTUsername = '04c261f8-2a92-4168-b486-d3a1daaa2f30';
+exports.STTPassword = 'TrFYkzEIPvTP';
+
+// Credentials for Watson Conversation service
+exports.ConUsername = 'fc3d75f5-effd-493e-9f12-d6c0549f843f';
+exports.ConPassword = '1a7c1BVb4Qf3';
+exports.ConWorkspace = 'f95b73f0-2d25-454e-a057-d20a60a724eb'
+
+//Credentials for Watson Tone Analyzer service
+exports.ToneUsername = '3855fb02-b1c2-4466-9aef-1bb76e666f24';
+exports.TonePassword = 'cMsYfX0IQezl';
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = '6834bf4c-a0ec-4b71-b42a-64ab856fe96e';
+exports.TTSPassword = 'FUwXfIcbzDR2';
